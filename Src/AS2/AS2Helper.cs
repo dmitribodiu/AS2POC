1d71d9a909fdde81cd8ef07c320cf332775cbc8d
Bogdan Belous <bogdan.belous@alvys.com>
2023-02-01 17:21:14 +0200

    added Cryptography and AS2HelperV2

--- Src/AS2/AS2Helper.cs
+++ Src/AS2/AS2Helper.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Org.BouncyCastle.Asn1.Ocsp;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
 using System.Security.Cryptography.Pkcs;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AS2
 {
@@ -27,7 +37,7 @@ namespace AS2
         private static byte[] Sign(byte[] message, string signerCertificate, string signerPassword)
         {
             var certificate = new X509Certificate2(signerCertificate, signerPassword);
-            var contentInfo = new ContentInfo(message);
+            var contentInfo = new System.Security.Cryptography.Pkcs.ContentInfo(message);
 
             var signedCms = new SignedCms(contentInfo, true);
             var cmsSigner = new CmsSigner(certificate);
@@ -41,9 +51,9 @@ namespace AS2
         private static byte[] Encrypt(byte[] message, string recipientCerteficate, string encryptionAlgorithm)
         {
             var certificate = new X509Certificate2(recipientCerteficate);
-            var contentInfo = new ContentInfo(message);
+            var contentInfo = new System.Security.Cryptography.Pkcs.ContentInfo(message);
 
-            var envelopedCms = new EnvelopedCms(contentInfo, new AlgorithmIdentifier(new System.Security.Cryptography.Oid(encryptionAlgorithm))); // should be 3DES or RC2
+            var envelopedCms = new EnvelopedCms(contentInfo, new System.Security.Cryptography.Pkcs.AlgorithmIdentifier(new System.Security.Cryptography.Oid(encryptionAlgorithm))); // should be 3DES or RC2
             var recipient = new CmsRecipient(SubjectIdentifierType.IssuerAndSerialNumber, certificate);
 
             envelopedCms.Encrypt(recipient);
@@ -73,5 +83,153 @@ namespace AS2
         {
             return file;
         }
+    }
+
+    public static class AS2HelperV2
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="recipientCert">Niagara</param>
+        /// <param name="senderPrivateKey">Alvys</param>
+        /// <returns></returns>
+        public static byte[] Package(string message, X509Certificate2 recipientCert, AsymmetricCipherKeyPair senderPrivateKey)
+        {
+            // Encrypt the message
+            byte[] encryptedMessage = Encrypt(Encoding.UTF8.GetBytes(message), recipientCert);
+            //byte[] encryptedMessage = Encoding.UTF8.GetBytes(message);
+
+            // Sign the encrypted message
+            byte[] signedMessage = Sign(encryptedMessage, senderPrivateKey);
+
+            // Verify the signature
+            var publicKey = (RsaKeyParameters)PublicKeyFactory.CreateKey(recipientCert.PublicKey.EncodedKeyValue.RawData);
+            bool isValidSignature = VerifySignature(signedMessage, publicKey);
+
+            Console.WriteLine("Original message: " + message);
+            Console.WriteLine("Encrypted message: " + Encoding.UTF8.GetString(encryptedMessage));
+            Console.WriteLine("Signed message: " + Encoding.UTF8.GetString(signedMessage));
+            Console.WriteLine("Signature validity: " + isValidSignature);
+
+            return signedMessage;
+        }
+
+        static byte[] Encrypt(byte[] data, X509Certificate2 recipientCertificate)
+        {
+            //RsaKeyParameters publicKey = LoadPublicKey(recipientPublicKeyPath);
+            //IBufferedCipher cipher = new Pkcs1Encoding(new RsaEngine());
+            //X509Certificate2 recipientCertificate = LoadCertificate(recipientCertificatePath);
+
+            var key = recipientCertificate.GetPublicKey();
+            var publicKey2 = PublicKeyFactory.CreateKey(key);
+            RsaKeyParameters publicKey = (RsaKeyParameters)PublicKeyFactory.CreateKey(recipientCertificate.PublicKey.EncodedKeyValue.RawData);
+
+            //var rsa = recipientCertificate.GetRSAPublicKey();
+            //var rsaParams = rsa.ExportParameters(false);
+            //var publicKey = new RsaKeyParameters(false, new BigInteger(rsaParams.Modulus), new BigInteger(rsaParams.Exponent));
+
+            //RsaKeyParameters key = (RsaKeyParameters)recipientCertificate.GetPublicKey();
+            //// Construct a microsoft RSA crypto service provider using the public key in the certificate
+            //RSAParameters param = new RSAParameters();
+            //param.Exponent = key.Exponent.ToByteArrayUnsigned();
+            //param.Modulus = key.Modulus.ToByteArrayUnsigned();
+
+            IBufferedCipher cipher = CipherUtilities.GetCipher("RSA/ECB/PKCS1Padding");
+            //IAsymmetricBlockCipher cipher = new Pkcs1Encoding(new RsaEngine());
+
+            cipher.Init(true, publicKey);
+            return cipher.DoFinal(data);
+        }
+
+        private static AsymmetricKeyParameter ToAsymmetricKeyParameter(X509Certificate2 certificate)
+        {
+            return PublicKeyFactory.CreateKey(certificate.GetPublicKey());
+        }
+
+        static byte[] Sign(byte[] data, AsymmetricCipherKeyPair keyPair)
+        {
+            //X509Certificate2 senderCertificate = LoadCertificate(senderCertificatePath, senderCertificatePassword);
+            //AsymmetricCipherKeyPair keyPair = LoadPrivateKey(senderPrivateKeyPath);
+
+            ISigner signer = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id);
+            signer.Init(true, keyPair.Private);
+            signer.BlockUpdate(data, 0, data.Length);
+            return signer.GenerateSignature();
+        }
+
+        static bool VerifySignature(byte[] signedData, RsaKeyParameters recipientPublicKey)
+        {
+            //X509Certificate2 recipientCertificate = LoadCertificate(recipientCertificatePath);
+            //AsymmetricKeyParameter publicKey = LoadPublicKey(recipientPublicKey);
+
+            ISigner signer = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id);
+
+            //var publicKey = recipientPublicKey.GetRSAPublicKey();
+            //var publicKey2 = recipientPublicKey.PublicKey.Key;
+
+
+            signer.Init(false, recipientPublicKey);
+            signer.BlockUpdate(signedData, 0, signedData.Length);
+            return signer.VerifySignature(signedData);
+
+            //signer.BlockUpdate(signedData, 0, signedData.Length - 128);
+            //return signer.VerifySignature(signedData, signedData.Length - 128, 128);
+
+            //signer.Init(false, recipientCertificate.PublicKey.Key);
+            //signer.BlockUpdate(signedData, 0, signedData.Length - 20);
+            //return signer.VerifySignature(signedData, signedData.Length - 20, 20);
+
+            ////Base64 Decode
+            //byte[] encodeBytes = UnicodeEncoding.ASCII.GetBytes(Signature);
+            //byte[] decodeBytes;
+            //using (MemoryStream decStream = new MemoryStream())
+            //{
+            //    base64.Decode(encodeBytes, 0, encodeBytes.Length, decStream);
+            //    decodeBytes = decStream.ToArray();
+            //}
+
+            //return signer.VerifySignature(decodeBytes);
+        }
+
+        //static X509Certificate2 LoadCertificate(string certificatePath, string password = null)
+        //{
+        //    return string.IsNullOrEmpty(password)
+        //        ? new X509Certificate2(certificatePath)
+        //        : new X509Certificate2(certificatePath, password);
+        //}
+
+    }
+
+    public static class AS2HelperV3
+    {
+        //public static void EncryptMessage(string message, X509Certificate recipientCertificate,
+        //AsymmetricKeyParameter recipientPrivateKey, X509Certificate senderCertificate,
+        //AsymmetricKeyParameter senderPrivateKey, out byte[] encryptedMessage, out byte[] signature)
+        //{
+        //    // Encrypt message
+        //    IBufferedCipher cipher = CipherUtilities.GetCipher("DES/CBC/PKCS5Padding");
+        //    KeyGenerationParameters keyGenerationParameters = new KeyGenerationParameters(new SecureRandom(), 128);
+        //    KeyParameter key = (KeyParameter)new Pkcs5S2KeyWrapper(keyGenerationParameters).GenerateDerivedMacParameters(64);
+        //    cipher.Init(true, new ParametersWithIV(key, key.GetKey()));
+        //    encryptedMessage = cipher.DoFinal(Encoding.UTF8.GetBytes(message));
+
+        //    // Sign message
+        //    ISigner signer = SignerUtilities.GetSigner("SHA256withRSA");
+        //    signer.Init(true, senderPrivateKey);
+        //    signer.BlockUpdate(encryptedMessage, 0, encryptedMessage.Length);
+        //    signature = signer.GenerateSignature();
+
+        //    // Create AS2 message
+        //    X509Certificate[] chain = new X509Certificate[] { senderCertificate };
+        //    X509CertificateStructure[] certStructs = Array.ConvertAll(chain, X509CertificateStructure.GetInstance);
+        //    As2SignedDataGenerator gen = new As2SignedDataGenerator();
+        //    gen.AddSignerInfoGenerator(new As2SignerInfoGenerator(senderPrivateKey, senderCertificate, "sha256").SetDirectSignature(true));
+        //    gen.AddCertificates(new X509Store(certStructs));
+        //    gen.AddSignature();
+        //    ApplicationPkcs7Mime pkcs7 = gen.Generate(new CmsProcessableByteArray(encryptedMessage), CmsSignedData.DontAddSignatureTimeStampToken);
+
+        //    // Send AS2 message        ...
+        //}
     }
 }
--- Src/AS2/Program.cs
+++ Src/AS2/Program.cs
@@ -1,13 +1,89 @@
-﻿using System;
+﻿using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AS2
 {
     internal class Program
     {
+        static string CertificateFolderPath = @"..\..\..\Certificates";
+
         static void Main(string[] args)
         {
-            var a = 53243;
             Console.WriteLine("Hello World!");
+
+            //var certificate = GenerateCertificate();
+
+            var publicNiagaraCert = new X509Certificate2($"{CertificateFolderPath}\\b2buatcrtsha256.cer");
+            //var publicNiagaraCert = new X509Certificate2($"{CertificateFolderPath}\\AH100-Alvys.cer");
+            var privateAlvysTenantKey = LoadPrivateKey($"{CertificateFolderPath}\\AH100-Alvys-private.pem");
+
+            var data = AS2HelperV2.Package("Test text 123", publicNiagaraCert, privateAlvysTenantKey);
+        }
+
+        static X509Certificate2 GenerateCertificate()
+        {
+            AsymmetricCipherKeyPair CertificateKey;
+            var X509RootCert = Cryptography.CreateCertificate("C=US, O=Alvys", "CN=ALVYS", 5, out CertificateKey);
+
+            var fileName = "AH100-Alvys";
+
+            //now let us write the certificates files to the folder 
+            File.WriteAllBytes($"{CertificateFolderPath}\\{fileName}.cer", X509RootCert.RawData);
+            //File.WriteAllBytes(folder + "\\" + "X509Cert.der", X509RootCert.RawData);
+
+            string PublicPEMFile = $"{CertificateFolderPath}\\{fileName}-public.pem";
+            string PrivatePEMFile = $"{CertificateFolderPath}\\{fileName}-private.pem";
+
+            //now let us also create the PEM file as well in case we need it
+            using (TextWriter textWriter = new StreamWriter(PublicPEMFile, false))
+            {
+                PemWriter pemWriter = new PemWriter(textWriter);
+                pemWriter.WriteObject(CertificateKey.Public);
+                pemWriter.Writer.Flush();
+            }
+
+            //now let us also create the PEM file as well in case we need it
+            using (TextWriter textWriter = new StreamWriter(PrivatePEMFile, false))
+            {
+                PemWriter pemWriter = new PemWriter(textWriter);
+                pemWriter.WriteObject(CertificateKey.Private);
+                pemWriter.Writer.Flush();
+            }
+
+            return X509RootCert;
+        }
+
+        /// <summary>
+        /// per file
+        /// </summary>
+        /// <param name="privateKeyPath"></param>
+        /// <returns></returns>
+        static AsymmetricCipherKeyPair LoadPrivateKey(string privateKeyPath)
+        {
+            using (TextReader reader = File.OpenText(privateKeyPath))
+            {
+                PemReader pemReader = new PemReader(reader);
+                return (AsymmetricCipherKeyPair)pemReader.ReadObject();
+            }
+        }
+
+        /// <summary>
+        /// pem file // AsymmetricKeyParameter publicKey = LoadPublicKey(recipientPublicKey);
+        /// </summary>
+        /// <param name="publicKeyPath"></param>
+        /// <returns></returns>
+        static RsaKeyParameters LoadPublicKey(string publicKeyPath)
+        {
+            using (TextReader reader = File.OpenText(publicKeyPath))
+            {
+                PemReader pemReader = new PemReader(reader);
+                AsymmetricKeyParameter publicKey = (AsymmetricKeyParameter)pemReader.ReadObject();
+                return (RsaKeyParameters)publicKey;
+            }
         }
     }
 }
